84c7c6d3d0fa9b6870ce4db37892ca637df33114
shweta1790 <shwetakatalkar17@.gmailcom>
2024-06-20 21:11:00 +0530

    first commit

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
 from .models import Tag, Author, Post
 
 class PostAdmin(admin.ModelAdmin):
